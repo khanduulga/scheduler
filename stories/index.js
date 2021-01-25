dd55d1759200ffaa34df54bef755119c24703105
Khanduulga Munkhbold <m.khanduulga@gmail.com>
2021-01-24 20:22:50 -0800

    Impletemented and added stories for Header and Empty for the Appointment component

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,13 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+
+
+
 
 
 
@@ -131,4 +138,18 @@ storiesOf("InterviewerList", module)
       setInterviewer={action("setInterviewer")}
     />
   ));
-  
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time="12pm"/>)
+  .add("Header", () => <Header time="12pm" />)
+  .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+  .add("Show", () => <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewers[0]}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    />)
